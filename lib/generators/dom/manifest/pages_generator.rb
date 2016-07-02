11382ee976e8bc44d0fc86176cbf9d829f894f3e
ibaciulis <i.baciulis@gmail.com>
2016-07-02 11:35:38 +0300

    Allow to define namespace and path

--- lib/generators/dom/manifest/pages_generator.rb
+++ lib/generators/dom/manifest/pages_generator.rb
@@ -1,15 +1,20 @@
 module Dom
   module Manifest
     class PagesGenerator < Rails::Generators::Base
+      class_option :path, type: 'string', default: 'app/assets/javascripts', description: 'Path where to save generated files'
+
       def create_pages_manifest
-        content = Dir.glob('app/assets/javascripts/pages/**/*.coffee').reverse.each_with_object('') do |path, o|
-          path.gsub!(/^app\/assets\/javascripts\//, '')
+        js_assets_path = options.path.gsub(/\/$/, '')
+        reg = Regexp.new("^#{js_assets_path}/")
+
+        content = Dir.glob("#{js_assets_path}/pages/**/*.coffee").reverse.each_with_object('') do |path, o|
+          path.gsub!(reg, '')
           path.gsub!(/(.js)?.coffee$/, '')
-          o << "#= require #{path}\n"
+          o << "#= require ./#{path}\n"
         end
 
-        FileUtils.rm('app/assets/javascripts/pages.js.coffee') if File.exist?('app/assets/javascripts/pages.js.coffee')
-        File.write('app/assets/javascripts/pages.coffee', content)
+        FileUtils.rm("#{js_assets_path}/pages.js.coffee") if File.exist?("#{js_assets_path}/pages.js.coffee")
+        File.write("#{js_assets_path}/pages.coffee", content)
       end
     end
   end
--- lib/generators/dom/page_generator.rb
+++ lib/generators/dom/page_generator.rb
@@ -3,6 +3,8 @@ module Dom
     class PageGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
       argument :file, type: :string
+      class_option :namespace, type: 'string', default: 'App', description: 'JS class namespace: App.Foobar, Admin.Foobar'
+      class_option :path, type: 'string', default: 'app/assets/javascripts', description: 'Path where to save generated files'
 
       def create_page_files
         parts = %w(pages) + file.split('/')
@@ -12,14 +14,14 @@ module Dom
         i = 1
         while i < total_parts
           class_file = parts[0..i].join('/') + '.coffee'
-          class_content = "class App.#{parts_camelized[0..i].join('.')} extends App.#{parts_camelized[0..i-1].join('.')}"
-          create_file "app/assets/javascripts/#{class_file}", class_content
+          class_content = "class #{options.namespace}.#{parts_camelized[0..i].join('.')} extends #{options.namespace}.#{parts_camelized[0..i-1].join('.')}"
+          create_file "#{options.path.gsub(/\/$/, '')}/#{class_file}", class_content
           i += 1
         end
       end
 
       def generate_pages_manifest
-        generate 'dom:manifest:pages'
+        generate "dom:manifest:pages --path #{options.path}"
       end
     end
   end
